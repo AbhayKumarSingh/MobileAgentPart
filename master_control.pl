8aa0e72370fe06a336146693e7c2e0c557ce99cc
Abhay Kumar Singh <abhayawesome@gmail.com>
2015-03-26 20:01:43 +0530

    Automates closing at the end of experiment.
    When an experiment ends, this commit adds functionalities of logging the
    end of experiment, and closing all the prolog instances throughout the
    network, including the master control.

--- master_control.pl
+++ master_control.pl
@@ -18,6 +18,11 @@ start_master:-
 
 start_master:- write(`~M~JError in Create Master Agent`).
 
+% To kill prolog instances throughout the network.
+kill_throughout_network:-
+			exec('runClient.bat','2',_),
+		!.
+
 % network control manages the genration and termination of the nodes in different
 % PCs (IPs)
 start_network_control:-
@@ -172,6 +177,13 @@ action(( startagent, Node, Type, Data ), List ):-
 	agent_create( master, PL, IP, TaskMPort ),
 	agent_post(master,PL,exec_sol(consult( 'initiation.pl' ))),
 	agent_post(master,PL,exec_sol( start_exe( Type, Data ))).
+
+action(( end, _, _, _ ), _ ):-
+	( write( `End of Experiment` ) ) ~> Avar,
+	send_log( Avar ),
+	kill_throughout_network,
+	exec('C:\Windows\System32\taskkill.exe', '/F /IM PRO386W.EXE /T', _).
+	% more to be done later.
 	
 start_agent( Node, Type, Data ):-
 	node_table( List ),
